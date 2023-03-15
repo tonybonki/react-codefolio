911181f15bed2a2cddf41afb06386ae52b9f7a42
Tony Bonki <114664225+tonybonki@users.noreply.github.com>
2023-03-15 11:32:39 +0000

    editing navbar links

---
+++ code-folio/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// Import Components
+import Navbar from './components/Navbar'
+import {
+  ChakraProvider,
+} from '@chakra-ui/react';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Navbar />
+    </ChakraProvider>
+  );
+}
+
+export default App;
