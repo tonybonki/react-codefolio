df7cc0725e1ecf0bb32ec84eecac8a9aadfcb5fb
Tony Bonki <114664225+tonybonki@users.noreply.github.com>
2023-03-21 11:25:23 +0000

    added Hero into webpage

--- code-folio/src/App.js
+++ code-folio/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // Import Components
+import MainHero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar'
+
 import {
   ChakraProvider,
 } from '@chakra-ui/react';
@@ -11,6 +13,7 @@ function App() {
   return (
     <ChakraProvider>
       <Navbar />
+      <MainHero />
     </ChakraProvider>
   );
 }
