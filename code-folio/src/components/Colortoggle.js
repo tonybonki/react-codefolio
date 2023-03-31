24e1101a3c6929c8ea738871c5fdf7287b9d06d8
Tony Bonki <114664225+tonybonki@users.noreply.github.com>
2023-03-31 07:48:40 +0000

    completed color toggle function in button componen

--- code-folio/src/components/Colortoggle.js
+++ code-folio/src/components/Colortoggle.js
@@ -4,17 +4,17 @@ import { BsSun, BsMoon } from 'react-icons/bs';
 export default function ColorModeToggle(props: ButtonProps) {
 const { colorMode, toggleColorMode } = useColorMode();
 
-// If Shift and N is pressed change the color mode to dark mode
+// If Shift and N is pressed toggle the color mode 
 const handleKeyDown = event => {
   if (event.shiftKey && event.code === 'KeyN') {
     toggleColorMode();
+    // This line of code prevents the website from 
+    // slowing down if the funtion is executed many times
+    document.removeEventListener('keydown', handleKeyDown);
   }
 };
-
-document.removeEventListener('keydown', handleKeyDown);
+  
 document.addEventListener('keydown', handleKeyDown);
-
-
 
   return (
     <Button
