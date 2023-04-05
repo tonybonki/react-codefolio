6c9a533d8c1ee90d6874d3e9392e9ef92d4392c9
tonybonki <tony.bonki2@mail.dcu.ie>
2023-04-05 18:54:59 +0100

    added color mode features in the project section

--- code-folio/src/components/Projects/Projects.js
+++ code-folio/src/components/Projects/Projects.js
@@ -8,6 +8,7 @@ import {
   Text,
   Stack,
   StackDivider,
+  useColorMode,
   Link,
   useMediaQuery,
   useColorModeValue,
@@ -19,15 +20,10 @@ import { ExternalLinkIcon } from '@chakra-ui/icons';
 // Import Project Data
 import { projects } from '../../data/data';
 
-
-const Skill = ({ text, image, }: SkillProps) => {
+const Skill = ({ text, image }: SkillProps) => {
   return (
     <Stack direction={'row'} align={'center'}>
-      <Flex
-        w={8}
-        h={8}
-        align={'center'}
-        justify={'center'}      >
+      <Flex w={8} h={8} align={'center'} justify={'center'}>
         {image}
       </Flex>
       <Text fontWeight={600}>{text}</Text>
@@ -38,6 +34,12 @@ const Skill = ({ text, image, }: SkillProps) => {
 export default function SplitWithImage() {
   const [isLargerThanMobile] = useMediaQuery('(max-width: 680px)');
   const [isSmallerThanMobile] = useMediaQuery('(min-width: 680px)');
+  // Color mode constants
+  const { colorMode } = useColorMode();
+  const textColor = { light: 'gray.600', dark: 'white.400' };
+  const dividerColor = { light: 'gray.200', dark: 'gray.600' };
+  const linkColor = { light: 'teal.400', dark: 'teal.200' };
+  const bgColor = { light: 'teal.50', dark: 'teal.800' };
 
   return (
     <Container maxW={'6xl'}>
@@ -50,52 +52,54 @@ export default function SplitWithImage() {
         and Projects
       </Heading>
       {/* Map Projects */}
-      {projects.map(project => (
+      {projects.map((project) => (
         <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
           <Stack spacing={4}>
-            <Heading color={'gray.600'} fontSize={30}>
-              {project.titleFname}{' '}
-              <Text as={'span'} color={project.colorTheme}>
+            <Heading color={textColor[colorMode]} fontSize={30}>
+              {project.titleFname}{" "}
+              <Text as={"span"} color={project.colorTheme}>
                 {project.titleSname}
               </Text>
             </Heading>
             <Text
-              color={'gray.600'}
-              fontSize={{ base: '13px', sm: '13px', lg: '15px' }}
+              color={textColor[colorMode]}
+              fontSize={{ base: "13px", sm: "13px", lg: "15px" }}
             >
               {project.description}
             </Text>
-            <Heading color={'gray.600'} fontSize={20}>
+            <Heading color={textColor[colorMode]} fontSize={20}>
               Built with
             </Heading>
             <Stack
               spacing={0.2}
-              divider={<StackDivider borderColor={'gray.200'} />}
+              divider={
+                <StackDivider borderColor={dividerColor[colorMode]} />
+              }
             >
-              {project.stack.map(tech => (
+              {project.stack.map((tech) => (
                 <Skill
                   image={<Image src={`/icons/${tech}.png`} w={4} h={4} />}
                   text={
                     <Text
                       fontWeight={500}
-                      fontSize={{ base: '11px', sm: '12px', lg: '13px' }}
-                      color={'gray.600'}
+                      fontSize={{ base: "11px", sm: "12px", lg: "13px" }}
+                      color={textColor[colorMode]}
                     >
                       {tech}
                     </Text>
                   }
                 />
               ))}
-              }
               <Link
-                color={'teal.400'}
+                color={linkColor[colorMode]}
+                colorScheme={'teal'}
                 fontWeight={600}
-                fontSize={'14px'}
-                bg={'teal.50'}
+                fontSize={"14px"}
+                bg={bgColor[colorMode]}
                 p={1}
                 marginTop={3}
-                alignSelf={'flex-start'}
-                rounded={'md'}
+                alignSelf={"flex-start"}
+                rounded={"md"}
                 // Replace this link with mapped link attribute
                 href={project.previewLink}
                 isExternal
@@ -107,8 +111,8 @@ export default function SplitWithImage() {
               <>
                 <Image
                   alt={`${project.imageName}`}
-                  width={'250px'}
-                  marginX={'auto !important'}
+                  width={"250px"}
+                  marginX={"auto !important"}
                   src={`/images/Busy Blogs/busy-blogs-mobile.png`}
                 />
               </>
@@ -119,7 +123,7 @@ export default function SplitWithImage() {
               <>
                 <Image
                   alt={`${project.imageName}`}
-                  objectFit={'contain'}
+                  objectFit={"contain"}
                   src={`/images/Busy Blogs/busy-blogs-macbook.png`}
                 />
               </>
