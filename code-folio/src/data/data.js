3b83cc069abf63f8f4ab3987850168da43db8fd8
tonybonki <tony.bonki2@mail.dcu.ie>
2023-04-06 19:15:26 +0100

    added tailwind css icon to react portfolio

--- code-folio/src/data/data.js
+++ code-folio/src/data/data.js
@@ -21,7 +21,7 @@ export const projects = [
     description:
       'A beautiful blog website with backend functionality built with MongoDB Node.js, Bootstrap 5.2 and EJS. This is just one of many websites that I will be creating to practice and also show off my fullstack website development skills. I called this project Busy Blogs because users will be able to create their own blog posts and post it onto the website.',
     githubLink: 'https://github.com/tonybonki/Busy-Blogs',
-    stack: ['React', 'Javascript', 'Bootstrap', 'MongoDB'],
+    stack: ['React', 'Javascript', 'TailwindCSS', 'MongoDB'],
     colorTheme: '#625698',
     previewLink: 'https://tony-bonki-portfolio.netlify.app/',
   },
