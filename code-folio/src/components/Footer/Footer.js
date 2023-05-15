89312545735c9c33302af6b8023056e3f11ba600
Tony Bonki <114664225+tonybonki@users.noreply.github.com>
2023-05-15 11:55:12 +0000

    deleted unused variables in footer.js

--- code-folio/src/components/Footer/Footer.js
+++ code-folio/src/components/Footer/Footer.js
@@ -1,6 +1,5 @@
 import {
   Box,
-  chakra,
   Container,
   Stack,
   Text,
@@ -8,46 +7,13 @@ import {
   Image,
   HStack,
   useColorModeValue,
-  VisuallyHidden,
 } from '@chakra-ui/react';
 
-import { ReactNode } from 'react';
 
 const Logo = (props: any) => {
   return <Image h={5} src="./logo.png"></Image>;
 };
 
-const SocialButton = ({
-  children,
-  label,
-  href,
-}: {
-  children: ReactNode,
-  label: string,
-  href: string,
-}) => {
-  return (
-    <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
-      w={8}
-      h={8}
-      cursor={'pointer'}
-      as={'a'}
-      href={href}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
-      _hover={{
-        color: 'teal.400',
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  );
-};
 
 export default function Footer() {
   return (
