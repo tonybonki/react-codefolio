df8cec7f30790643674e4bf68a559b1c839aae4c
Tony Bonki <114664225+tonybonki@users.noreply.github.com>
2023-05-15 14:16:52 +0000

    added skill icons and changed skill title colors

--- code-folio/src/components/WorkExperience/Skills.js
+++ code-folio/src/components/WorkExperience/Skills.js
@@ -1,15 +1,32 @@
-import { Box, SimpleGrid, Text, Stack, VStack, Heading, useColorModeValue, Divider} from '@chakra-ui/react';
-
-const Skill = ({ title, text }: SkillProps) => {
+import {
+  Box,
+  SimpleGrid,
+  HStack,
+  Text,
+  Stack,
+  VStack,
+  Heading,
+  useColorModeValue,
+  Icon,
+  Divider,
+} from '@chakra-ui/react';
+import { GoFileCode, GoPencil, GoMortarBoard } from 'react-icons/go';
+const Skill = ({ title, text, icon }: SkillProps) => {
   return (
     <Stack>
-      <Text
-        color={useColorModeValue('teal.500', 'teal.300')}
-        fontSize={24}
-        fontWeight={500}
-      >
-        {title}
-      </Text>
+      <HStack spacing={0}>
+        <Icon color={useColorModeValue('teal.500', 'teal.300')} boxSize={10}>
+          {icon}
+        </Icon>
+        <Text
+          color={useColorModeValue('gray.600', 'white')}
+          fontSize={24}
+          fontWeight={500}
+        >
+          {title}
+        </Text>
+      </HStack>
+
       <Text color={useColorModeValue('gray.600', 'white')}>{text}</Text>
     </Stack>
   );
@@ -39,15 +56,18 @@ export default function Skillset() {
             text={
               'As a website developer, I bring a unique combination of technical expertise and creative problem-solving skills to the table. I have a deep understanding of web development technologies such as HTML, CSS, JavaScript, and various CMS platforms'
             }
+            icon={<GoFileCode />}
           />
           <Skill
             title={'Visually stunning and user-friendly website development'}
+            icon={<GoPencil />}
             text={
               'I am confident in my ability to create visually stunning and user-friendly websites that meet the needs of clients and exceed their expectations.'
             }
           />
           <Skill
             title={'Creative thinker and fast learner'}
+            icon={<GoMortarBoard />}
             text={
               'Moreover, I am a fast learner and a creative thinker. I have the ability to troubleshoot and resolve issues quickly, ensuring that the site is always running smoothly.'
             }
