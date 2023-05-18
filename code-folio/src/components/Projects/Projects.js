560a2f4bf4464d81e95187d2108996553ef9ae6a
tonybonki <tony.bonki2@mail.dcu.ie>
2023-05-18 13:14:15 +0100

    completed filter functionality and component

--- code-folio/src/components/Projects/Projects.js
+++ code-folio/src/components/Projects/Projects.js
@@ -19,6 +19,7 @@ import {
   TabList,
   TabPanels,
   Tab,
+  Tooltip,
   VStack,
   TabPanel,
 } from '@chakra-ui/react';
@@ -56,7 +57,11 @@ export default function SplitWithImage() {
   const { colorMode } = useColorMode();
   const [isLargerThanMobile] = useMediaQuery('(max-width: 680px)');
   const [isSmallerThanMobile] = useMediaQuery('(min-width: 680px)');
-  const [activeTab, setActiveTab] = useState(0);
+  const [filter, setFilter] = useState('');
+
+  const filteredProjects = projects.filter(project =>
+    project.frameWork.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <Container
@@ -171,21 +176,20 @@ export default function SplitWithImage() {
       {/* Tab componet that filters projects bsaed on the programming language */}
       <Tabs variant="enclosed">
         <TabList>
-          <Tab onClick={() => setActiveTab(0)}>
+          <Tab onClick={() => setFilter('')}>
+            <VStack>
+              <Image h={5} src="/icons/menu.png"></Image>
+              {/* TBD have below feature or not */}
+              {/* <Text color={useColorModeValue('gray.600', 'white')}  fontWeight={500}>{' '}</Text>  */}
+            </VStack>
+          </Tab>
+          <Tab onClick={() => setFilter('React')}>
             <VStack>
               <Image h={5} src="/icons/React.png"></Image>
               {/* TBD have below feature or not */}
               {/* <Text color={useColorModeValue('gray.600', 'white')}  fontWeight={500}>{' '}</Text>  */}
             </VStack>
           </Tab>
-          <Tab onClick={() => setActiveTab(0)}>
-            <VStack>
-              <Image h={5} src="/icons/React.png"></Image>
-              {/* TBD have below feature or not */}
-              {/* <Text color={useColorModeValue('gray.600', 'white')}  fontWeight={500}>{' '}</Text>  */}
-            </VStack>
-          </Tab>
-          <Tab onClick={() => setActiveTab(1)}>Two</Tab>
         </TabList>
         <TabPanels>
           <TabPanel>{/* <p>one!</p> */}</TabPanel>
@@ -193,123 +197,121 @@ export default function SplitWithImage() {
         </TabPanels>
       </Tabs>
       {/* Map Projects */}
-      {projects
-        .filter((project, index) => activeTab === index)
-        .map(project => (
-          <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-            <Stack spacing={4}>
-              <Heading color={textColor[colorMode]} fontSize={30}>
-                {project.titleFname}{' '}
-                <Text as={'span'} color={project.colorTheme}>
-                  {project.titleSname}
-                </Text>
-              </Heading>
-              <Text
-                color={textColor[colorMode]}
-                fontSize={{ base: '13px', sm: '13px', lg: '15px' }}
-              >
-                {project.description}
+      {filteredProjects.map((project, index) => (
+        <SimpleGrid key={index} columns={{ base: 1, md: 2 }} spacing={10}>
+          <Stack spacing={4}>
+            <Heading color={textColor[colorMode]} fontSize={30}>
+              {project.titleFname}{' '}
+              <Text as={'span'} color={project.colorTheme}>
+                {project.titleSname}
               </Text>
-              <Heading color={textColor[colorMode]} fontSize={20}>
-                Software Stack
-              </Heading>
-              <Stack
-                spacing={0.2}
-                divider={<StackDivider borderColor={dividerColor[colorMode]} />}
-              >
-                {project.stack.map(tech => (
-                  <Skill
-                    image={<Image src={`/icons/${tech}.png`} w={4} h={4} />}
-                    text={
-                      <Text
-                        fontWeight={500}
-                        fontSize={{ base: '11px', sm: '12px', lg: '13px' }}
-                        color={textColor[colorMode]}
-                      >
-                        {tech}
-                      </Text>
-                    }
-                  />
-                ))}
-                <Box mt={4}>
-                  <Link
-                    color={linkColor[colorMode]}
-                    colorScheme={'teal'}
-                    fontWeight={600}
-                    fontSize={'14px'}
-                    marginRight={'10px'}
-                    bg={bgColor[colorMode]}
-                    p={1}
-                    marginTop={3}
-                    alignSelf={'flex-start'}
-                    rounded={'md'}
-                    // Replace this link with mapped link attribute
-                    href={project.previewLink}
-                    isExternal
-                  >
-                    Live Preview <ExternalLinkIcon mx="2px" />
-                  </Link>
-                  <Link
-                    color={linkColor[colorMode]}
-                    colorScheme={'teal'}
-                    fontWeight={600}
-                    fontSize={'14px'}
-                    bg={bgColor[colorMode]}
-                    p={1}
-                    isExternal
-                    marginTop={3}
-                    alignSelf={'flex-start'}
-                    rounded={'md'}
-                    href={project.githubLink}
-                  >
-                    Github Repository <ExternalLinkIcon mx="2px" />
-                  </Link>
-                </Box>
-              </Stack>
-              {isLargerThanMobile && (
+            </Heading>
+            <Text
+              color={textColor[colorMode]}
+              fontSize={{ base: '13px', sm: '13px', lg: '15px' }}
+            >
+              {project.description}
+            </Text>
+            <Heading color={textColor[colorMode]} fontSize={20}>
+              Software Stack
+            </Heading>
+            <Stack
+              spacing={0.2}
+              divider={<StackDivider borderColor={dividerColor[colorMode]} />}
+            >
+              {project.stack.map(tech => (
+                <Skill
+                  image={<Image src={`/icons/${tech}.png`} w={4} h={4} />}
+                  text={
+                    <Text
+                      fontWeight={500}
+                      fontSize={{ base: '11px', sm: '12px', lg: '13px' }}
+                      color={textColor[colorMode]}
+                    >
+                      {tech}
+                    </Text>
+                  }
+                />
+              ))}
+              <Box mt={4}>
+                <Link
+                  color={linkColor[colorMode]}
+                  colorScheme={'teal'}
+                  fontWeight={600}
+                  fontSize={'14px'}
+                  marginRight={'10px'}
+                  bg={bgColor[colorMode]}
+                  p={1}
+                  marginTop={3}
+                  alignSelf={'flex-start'}
+                  rounded={'md'}
+                  // Replace this link with mapped link attribute
+                  href={project.previewLink}
+                  isExternal
+                >
+                  Live Preview <ExternalLinkIcon mx="2px" />
+                </Link>
+                <Link
+                  color={linkColor[colorMode]}
+                  colorScheme={'teal'}
+                  fontWeight={600}
+                  fontSize={'14px'}
+                  bg={bgColor[colorMode]}
+                  p={1}
+                  isExternal
+                  marginTop={3}
+                  alignSelf={'flex-start'}
+                  rounded={'md'}
+                  href={project.githubLink}
+                >
+                  Github Repository <ExternalLinkIcon mx="2px" />
+                </Link>
+              </Box>
+            </Stack>
+            {isLargerThanMobile && (
+              <>
+                <Image
+                  alt={`${project.imageName}`}
+                  width={'250px'}
+                  marginX={'auto !important'}
+                  src={`./${project.mobileImageName}`}
+                />
+              </>
+            )}
+          </Stack>
+          <Flex
+            flex={1}
+            justify={'center'}
+            align={'center'}
+            position={'relative'}
+            w={'full'}
+          >
+            <Blob
+              w={'100%'}
+              h={'100%'}
+              position={'absolute'}
+              left={0}
+              opacity={'40%'}
+              zIndex={-1}
+              display={{ base: 'none', sm: 'block', lg: 'block' }}
+              color={project.colorTheme}
+            />
+            {/* The Image of Myself */}
+            <Box position={'relative'} m={3} overflow={'hidden'}>
+              {isSmallerThanMobile && (
                 <>
                   <Image
                     alt={`${project.imageName}`}
-                    width={'250px'}
-                    marginX={'auto !important'}
-                    src={`./${project.mobileImageName}`}
+                    objectFit={'contain'}
+                    src={`./${project.macbookImageName}`}
                   />
                 </>
               )}
-            </Stack>
-            <Flex
-              flex={1}
-              justify={'center'}
-              align={'center'}
-              position={'relative'}
-              w={'full'}
-            >
-              <Blob
-                w={'100%'}
-                h={'100%'}
-                position={'absolute'}
-                left={0}
-                opacity={'40%'}
-                zIndex={-1}
-                display={{ base: 'none', sm: 'block', lg: 'block' }}
-                color={project.colorTheme}
-              />
-              {/* The Image of Myself */}
-              <Box position={'relative'} m={3} overflow={'hidden'}>
-                {isSmallerThanMobile && (
-                  <>
-                    <Image
-                      alt={`${project.imageName}`}
-                      objectFit={'contain'}
-                      src={`./${project.macbookImageName}`}
-                    />
-                  </>
-                )}
-              </Box>
-            </Flex>
-            <Spacer />
-          </SimpleGrid>
-        ))}
+            </Box>
+          </Flex>
+          <Spacer />
+        </SimpleGrid>
+      ))}
     </Container>
   );
 }
